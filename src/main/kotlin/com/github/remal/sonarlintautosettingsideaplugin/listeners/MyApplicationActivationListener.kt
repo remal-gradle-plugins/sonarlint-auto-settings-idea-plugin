03e810bff71b95f79a30aa89654f854ce2bdafc8
GitHub Action <action@github.com>
2024-10-18 20:21:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/remal/sonarlintautosettingsideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/remal/sonarlintautosettingsideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.remal.sonarlintautosettingsideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
